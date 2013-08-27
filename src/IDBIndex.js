dffe0b8ce55c101a3219e5950294b753df4d3ff4
Parashuram <code@nparashuram.com>
2013-08-26 21:03:57 -0700

    Fix #74

--- src/IDBIndex.js
+++ src/IDBIndex.js
@@ -100,7 +100,7 @@
             idbModules.DEBUG && console.log("Trying to fetch data for Index", sql.join(" "), sqlValues);
             tx.executeSql(sql.join(" "), sqlValues, function(tx, data){
                 var d;
-                if (typeof opType === "count") {
+                if (opType === "count") {
                     d = data.rows.length;
                 }
                 else 
