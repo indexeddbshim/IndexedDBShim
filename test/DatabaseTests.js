88177d69699e721a0d7319bdbe38d8fc9a0cc564
Mariano Guerra <luismarianoguerra@gmail.com>
2014-07-30 17:30:30 +0200

    add failing test that opens same database multiple times

--- test/DatabaseTests.js
+++ test/DatabaseTests.js
@@ -56,3 +56,96 @@ queuedAsyncTest("Opening a database with a version " + dbVersion, function(){
 		stop();
 	};
 });
+
+queuedAsyncTest("Creating Many Stores with Indexes opening the same db every time", function(){
+    var dbOpenRequest,
+        version = ++dbVersion,
+        i,
+        stores = [],
+        indexes = [
+            {name: "IntIndex", key: "Int", options: {
+                "unique": false,
+                "multiEntry": false
+            }},
+            {name: "StringIndex", key: "String"}
+        ];
+
+    function makeOnSuccess(dbOpenRequest) {
+        return function(e){
+            ok(true, "Database Opened successfully");
+            _("Database opened successfully with version");
+            dbOpenRequest.result.close();
+        };
+    }
+
+    function makeOnError(dbOpenRequest) {
+        return function(e){
+            ok(false, "Database NOT Opened successfully");
+            _("Database NOT opened successfully");
+            nextTest();
+            start();
+        };
+    }
+
+    function makeOnUpgrade(dbOpenRequest) {
+        return function(e){
+            ok(true, "Database Upgraded successfully");
+            _("Database upgrade called");
+            var db = dbOpenRequest.result,
+            storeName = DB.OBJECT_STORE_1 + '_' + i,
+            objectStore = db.createObjectStore(storeName);
+
+            function createIndex(objectStore, index) {
+                return objectStore.createIndex(index.name, index.key, index.options);
+            }
+
+            stores.push(objectStore);
+
+            var j, len, index;
+            for (j = 0, len = indexes.length; j < len; j += 1) {
+                index = indexes[j];
+                createIndex(objectStore, index);
+            }
+
+            equal(objectStore.indexNames.length, indexes.length, "Indexes on object store successfully created");
+            _(objectStore.indexNames);
+        };
+    }
+
+    function makeOnBlocked(dbOpenRequest) {
+        return function(e){
+            _("Opening database blocked");
+            ok(false, "Opening database blocked");
+            start();
+            stop();
+        };
+    }
+
+    for (i = 0; i < 10; i += 1) {
+        dbOpenRequest = window.indexedDB.open(DB.NAME, version);
+        dbOpenRequest.onsuccess = makeOnSuccess(dbOpenRequest);
+        dbOpenRequest.onerror = makeOnError(dbOpenRequest);
+        dbOpenRequest.onupgradeneeded = makeOnUpgrade(dbOpenRequest);
+        dbOpenRequest.onblocked = makeOnBlocked(dbOpenRequest);
+    }
+
+    function waitForStoresReady(stores) {
+        if (stores.length === 0) {
+            nextTest();
+            start();
+        } else {
+            console.info('waiting for stores to be ready', stores);
+            stores[0].__waitForReady(function () {
+                setTimeout(function () {
+                    waitForStoresReady(stores.slice(1));
+                }, 1000);
+            });
+        }
+    }
+
+    waitForStoresReady(stores);
+
+    start();
+    stop();
+});
+
