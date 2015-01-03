0eca2419c6c5cd5226919f10ae1e000d0f0fd600
Mariano Guerra <mariano@marianoguerra.org>
2015-01-03 20:53:37 +0100

    new build fix, sync IDBCursor with upstream

--- src/IDBCursor.js
+++ src/IDBCursor.js
@@ -62,11 +62,12 @@
             sql.push("AND " + me.__keyColumnName + " >= ?");
             sqlValues.push(idbModules.Key.encode(me.__lastKeyContinued));
         }
-        // Determine the ORDER BY direction based on the cursor.
-        var direction = me.direction == 'prev' || me.direction == 'prevunique' ? 'DESC' : 'ASC';
 
-        sql.push("ORDER BY " + me.__keyColumnName + " " + direction);
-        sql.push("LIMIT 1 OFFSET " + me.__offset);
+        // Determine the ORDER BY direction based on the cursor.
+        var direction = me.direction === 'prev' || me.direction === 'prevunique' ? 'DESC' : 'ASC';
+
+        sql.push("ORDER BY ", me.__keyColumnName, " " + direction);
+        sql.push("LIMIT " + recordsToLoad + " OFFSET " + me.__offset);
         idbModules.DEBUG && console.log(sql.join(" "), sqlValues);
 
         me.__prefetchedData = null;
