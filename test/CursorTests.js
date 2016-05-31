c9e610516343a52d6d71bf44a3e3ad0a5137c597
Brett Zamir <brettz9@yahoo.com>
2016-05-31 13:36:49 -0700

    Allow QUnit tests to pass when "Check for globals" enabled (put certain test code blocks in closures)

--- test/CursorTests.js
+++ test/CursorTests.js
@@ -1,124 +1,128 @@
-function openObjectStore(name, storeName, callback){
-    queuedAsyncTest(name, function(){
-        var dbOpenRequest = window.indexedDB.open(DB.NAME);
-        dbOpenRequest.onsuccess = function(e){
-            _("Database opened successfully");
-            var db = dbOpenRequest.result;
-            var transaction = db.transaction([DB.OBJECT_STORE_1, DB.OBJECT_STORE_2], "readwrite");
-            var objectStore = transaction.objectStore(DB.OBJECT_STORE_1);
-            callback(objectStore);
+
+(function () {
+
+    function openObjectStore(name, storeName, callback){
+        queuedAsyncTest(name, function(){
+            var dbOpenRequest = window.indexedDB.open(DB.NAME);
+            dbOpenRequest.onsuccess = function(e){
+                _("Database opened successfully");
+                var db = dbOpenRequest.result;
+                var transaction = db.transaction([DB.OBJECT_STORE_1, DB.OBJECT_STORE_2], "readwrite");
+                var objectStore = transaction.objectStore(DB.OBJECT_STORE_1);
+                callback(objectStore);
+            };
+            dbOpenRequest.onerror = function(e){
+                ok(false, "Database NOT Opened successfully");
+                _("Database NOT opened successfully");
+                start();
+                nextTest();
+            };
+        });
+    }
+
+    queuedModule("Cursor");
+
+    openObjectStore("Iterating over cursor", DB.OBJECT_STORE_1, function(objectStore){
+        var cursorReq = objectStore.openCursor();
+        var count = 0;
+        cursorReq.onsuccess = function(e){
+            var cursor = cursorReq.result;
+            if (cursor) {
+                count ++;
+                ok(true, "Iterating over cursor " + cursor.key + " for value " + JSON.stringify(cursor.value));
+                cursor["continue"]();
+            }
+            else {
+                equal(count, 15);
+                objectStore.transaction.db.close();
+                start();
+                nextTest();
+            }
         };
-        dbOpenRequest.onerror = function(e){
-            ok(false, "Database NOT Opened successfully");
-            _("Database NOT opened successfully");
+        cursorReq.onerror = function(e){
+            _("Error on cursor request");
+            ok(false, "Could not continue opening cursor");
             start();
             nextTest();
         };
     });
-}
-
-queuedModule("Cursor");
-
-openObjectStore("Iterating over cursor", DB.OBJECT_STORE_1, function(objectStore){
-    var cursorReq = objectStore.openCursor();
-    var count = 0;
-    cursorReq.onsuccess = function(e){
-        var cursor = cursorReq.result;
-        if (cursor) {
-            count ++;
-            ok(true, "Iterating over cursor " + cursor.key + " for value " + JSON.stringify(cursor.value));
-            cursor["continue"]();
-        }
-        else {
-            equal(count, 15);
-            objectStore.transaction.db.close();
-            start();
-            nextTest();
-        }
-    };
-    cursorReq.onerror = function(e){
-        _("Error on cursor request");
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
-openObjectStore("Updating using a cursor", DB.OBJECT_STORE_1, function(objectStore){
-    var cursorReq = objectStore.openCursor();
-    cursorReq.onsuccess = function(e){
-        var cursor = cursorReq.result;
-        if (cursor) {
-            if (cursor.value.Int % 3 === 0) {
-                cursor.value.cursorUpdate = true;
-                var updateReq = cursor.update(cursor.value);
-                updateReq.onsuccess = function(){
-                    equal(cursor.key, updateReq.result, "Update value " + cursor.key);
-                    _("Updated cursor with key " + updateReq.result);
+    openObjectStore("Updating using a cursor", DB.OBJECT_STORE_1, function(objectStore){
+        var cursorReq = objectStore.openCursor();
+        cursorReq.onsuccess = function(e){
+            var cursor = cursorReq.result;
+            if (cursor) {
+                if (cursor.value.Int % 3 === 0) {
+                    cursor.value.cursorUpdate = true;
+                    var updateReq = cursor.update(cursor.value);
+                    updateReq.onsuccess = function(){
+                        equal(cursor.key, updateReq.result, "Update value " + cursor.key);
+                        _("Updated cursor with key " + updateReq.result);
+                        cursor["continue"]();
+                    };
+                    updateReq.onerror = function(){
+                        ok(false, "No Update " + cursor.key);
+                        cursor["continue"]();
+                    };
+                }
+                else {
+                    ok(true, "Got cursor value " + cursor.key + ":" + JSON.stringify(cursor.value));
                     cursor["continue"]();
-                };
-                updateReq.onerror = function(){
-                    ok(false, "No Update " + cursor.key);
-                    cursor["continue"]();
-                };
+                }
             }
             else {
-                ok(true, "Got cursor value " + cursor.key + ":" + JSON.stringify(cursor.value));
-                cursor["continue"]();
+                _("Iterating over all objects completed");
+                objectStore.transaction.db.close();
+                start();
+                nextTest();
             }
-        }
-        else {
-            _("Iterating over all objects completed");
-            objectStore.transaction.db.close();
+        };
+        cursorReq.onerror = function(e){
+            _("Error on cursor request");
+            ok(false, "Could not continue opening cursor");
             start();
             nextTest();
-        }
-    };
-    cursorReq.onerror = function(e){
-        _("Error on cursor request");
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
+        };
+    });
 
 
-openObjectStore("Deleting using a cursor", DB.OBJECT_STORE_1, function(objectStore){
-    var cursorReq = objectStore.openCursor();
-    var totalRows  = 15;
-    var cursorIteration = 0;
-    cursorReq.onsuccess = function(e){
-        var cursor = cursorReq.result;
-        if (cursor) {
-            cursorIteration++;
-            if (cursor.value.Int % 5 === 0) {
-                var updateReq = cursor["delete"]();
-                updateReq.onsuccess = function(){
-                    equal(undefined, updateReq.result, "Deleted value " + cursor.key);
-                    _("Deleted cursor with key " + updateReq.result);
+    openObjectStore("Deleting using a cursor", DB.OBJECT_STORE_1, function(objectStore){
+        var cursorReq = objectStore.openCursor();
+        var totalRows  = 15;
+        var cursorIteration = 0;
+        cursorReq.onsuccess = function(e){
+            var cursor = cursorReq.result;
+            if (cursor) {
+                cursorIteration++;
+                if (cursor.value.Int % 5 === 0) {
+                    var updateReq = cursor["delete"]();
+                    updateReq.onsuccess = function(){
+                        equal(undefined, updateReq.result, "Deleted value " + cursor.key);
+                        _("Deleted cursor with key " + updateReq.result);
+                        cursor["continue"]();
+                    };
+                    updateReq.onerror = function(){
+                        ok(false, "No delete " + cursor.key);
+                        cursor["continue"]();
+                    };
+                }
+                else {
+                    ok(true, "Got cursor value " + cursor.key + ":" + JSON.stringify(cursor.value));
                     cursor["continue"]();
-                };
-                updateReq.onerror = function(){
-                    ok(false, "No delete " + cursor.key);
-                    cursor["continue"]();
-                };
+                }
             }
             else {
-                ok(true, "Got cursor value " + cursor.key + ":" + JSON.stringify(cursor.value));
-                cursor["continue"]();
+                objectStore.transaction.db.close();
+                equal(cursorIteration, totalRows, "All cursors iterated");
+                _("Iterating over all objects completed");
+                start();
+                nextTest();
             }
-        }
-        else {
-            objectStore.transaction.db.close();
-            equal(cursorIteration, totalRows, "All cursors iterated");
-            _("Iterating over all objects completed");
+        };
+        cursorReq.onerror = function(e){
+            _("Error on cursor request");
+            ok(false, "Could not continue opening cursor");
             start();
             nextTest();
-        }
-    };
-    cursorReq.onerror = function(e){
-        _("Error on cursor request");
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
+        };
+    });
+}());
--- test/IndexTests.js
+++ test/IndexTests.js
@@ -37,210 +37,213 @@ queuedAsyncTest("Creating Indexes", function(){
     };
 });
 
-function openObjectStore(name, storeName, callback){
-    queuedAsyncTest(name, function(){
-        var dbOpenRequest = window.indexedDB.open(DB.NAME);
-        dbOpenRequest.onsuccess = function(e){
-            _("Database opened successfully");
-            var db = dbOpenRequest.result;
-            var transaction = db.transaction([DB.OBJECT_STORE_1, DB.OBJECT_STORE_2], "readwrite");
-            var objectStore = transaction.objectStore(DB.OBJECT_STORE_1);
-            callback(objectStore, db);
+(function () {
+
+    function openObjectStore(name, storeName, callback){
+        queuedAsyncTest(name, function(){
+            var dbOpenRequest = window.indexedDB.open(DB.NAME);
+            dbOpenRequest.onsuccess = function(e){
+                _("Database opened successfully");
+                var db = dbOpenRequest.result;
+                var transaction = db.transaction([DB.OBJECT_STORE_1, DB.OBJECT_STORE_2], "readwrite");
+                var objectStore = transaction.objectStore(DB.OBJECT_STORE_1);
+                callback(objectStore, db);
+            };
+            dbOpenRequest.onerror = function(e){
+                ok(false, "Database NOT Opened successfully");
+                _("Database NOT opened successfully");
+                start();
+                nextTest();
+            };
+        });
+    }
+
+    openObjectStore("Check index exists after reopening database", DB.OBJECT_STORE_1, function(objectStore, db){
+        equal(objectStore.indexNames.length, 2, "2 Indexes on still exist");
+        _(objectStore.indexNames);
+        db.close();
+        start();
+        nextTest();
+    });
+
+    openObjectStore("Check index keyPath exists after reopening database", DB.OBJECT_STORE_1, function(objectStore, db){
+        var index = objectStore.index("Int Index");
+        equal(index.keyPath, "Int", "keyPath on index still exists");
+        db.close();
+        start();
+        nextTest();
+    });
+
+    var key = sample.integer();
+    var value = sample.obj();
+    openObjectStore("Adding data after index is created", DB.OBJECT_STORE_1, function(objectStore, db){
+        var addReq = objectStore.add(value, key);
+        addReq.onsuccess = function(e){
+            equal(key, addReq.result, "Data successfully added");
+            _("Added to datastore with index " + key);
+            db.close();
+            start();
+            nextTest();
         };
-        dbOpenRequest.onerror = function(e){
-            ok(false, "Database NOT Opened successfully");
-            _("Database NOT opened successfully");
+        addReq.onerror = function(){
+            db.close();
+            ok(false, "Could not add data");
             start();
             nextTest();
         };
     });
-}
-
-openObjectStore("Check index exists after reopening database", DB.OBJECT_STORE_1, function(objectStore, db){
-    equal(objectStore.indexNames.length, 2, "2 Indexes on still exist");
-    _(objectStore.indexNames);
-    db.close();
-    start();
-    nextTest();
-});
-
-openObjectStore("Check index keyPath exists after reopening database", DB.OBJECT_STORE_1, function(objectStore, db){
-    var index = objectStore.index("Int Index");
-    equal(index.keyPath, "Int", "keyPath on index still exists");
-    db.close();
-    start();
-    nextTest();
-});
-
-var key = sample.integer();
-var value = sample.obj();
-openObjectStore("Adding data after index is created", DB.OBJECT_STORE_1, function(objectStore, db){
-    var addReq = objectStore.add(value, key);
-    addReq.onsuccess = function(e){
-        equal(key, addReq.result, "Data successfully added");
-        _("Added to datastore with index " + key);
-        db.close();
-        start();
-        nextTest();
-    };
-    addReq.onerror = function(){
-        db.close();
-        ok(false, "Could not add data");
-        start();
-        nextTest();
-    };
-});
-openObjectStore("Index Cursor", DB.OBJECT_STORE_1, function(objectStore, db){
-    var index = objectStore.index("Int Index");
-    var indexCursorReq = index.openCursor();
-    indexCursorReq.onsuccess = function(){
-        var cursor = indexCursorReq.result;
-        if (cursor) {
-            _("Iterating over cursor " + cursor.key + " for value " + JSON.stringify(cursor.value));
-            cursor["continue"]();
-        }
-        else {
-            ok(true, "Cursor Iteration completed");
-            db.close();
-            start();
-            nextTest();
-        }
-    };
-    indexCursorReq.onerror = function(){
-        _("Error on cursor request");
-        db.close();
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
-
-openObjectStore("Index Key Cursor", DB.OBJECT_STORE_1, function(objectStore, db){
-    var index = objectStore.index("Int Index");
-    var indexCursorReq = index.openKeyCursor();
-    indexCursorReq.onsuccess = function(){
-        var cursor = indexCursorReq.result;
-        if (cursor) {
-            _("Iterating over cursor " + cursor.key + " for value " + JSON.stringify(cursor.value));
-            cursor["continue"]();
-        }
-        else {
-            ok(true, "Cursor Iteration completed");
-            db.close();
-            start();
-            nextTest();
-        }
-    };
-    indexCursorReq.onerror = function(){
-        _("Error on cursor request");
-        db.close();
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
-
-openObjectStore("Index Get", DB.OBJECT_STORE_1, function(objectStore, db){
-    var index = objectStore.index("Int Index");
-    var req = index.get(value.Int);
-    req.onsuccess = function(){
-        deepEqual(req.result, value, "Got object from Index Get");
-        console.log("Got ", req.result, value);
-        db.close();
-        start();
-        nextTest();
-    };
-    req.onerror = function(){
-        _("Error on cursor request");
-        db.close();
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
-
-
-openObjectStore("Index Get Key", DB.OBJECT_STORE_1, function(objectStore, db){
-    var index = objectStore.index("Int Index");
-    var req = index.getKey(value.Int);
-    req.onsuccess = function(){
-        equal(req.result, key, "Got key from Index Get");
-        console.log("Got ", req.result, value);
-        db.close();
-        start();
-        nextTest();
-    };
-    req.onerror = function(){
-        _("Error on cursor request");
-        db.close();
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
-
-openObjectStore("Index update Cursor", DB.OBJECT_STORE_1, function(objectStore, db){
-    var index = objectStore.index("Int Index");
-    var kr = IDBKeyRange.only(value.Int);
-    var indexCursorReq;
-    try {
-        indexCursorReq = index.openCursor(kr);
-    } catch (err) {
-        if (err.name === 'DataError') { // PhantomJS 2.2.1 having issue here
-            // as mistakenly confusing supplied key range here with supplying
-            // key to put/add (when in-line keys are used)
-            db.close();
-            ok(false, "Cursor update failed; possibly PhantomJS 2.2.1 bug");
-            start();
-            nextTest();
-            return;
-        }
-        throw err;
-    }
-    indexCursorReq.onsuccess = function(){
-        var cursor = indexCursorReq.result;
-        if (cursor) {
-            var cursorValue = cursor.value;
-            cursorValue.updated = true;
-            var updateReq = cursor.update(cursorValue);
-            updateReq.onerror = function() {
+    openObjectStore("Index Cursor", DB.OBJECT_STORE_1, function(objectStore, db){
+        var index = objectStore.index("Int Index");
+        var indexCursorReq = index.openCursor();
+        indexCursorReq.onsuccess = function(){
+            var cursor = indexCursorReq.result;
+            if (cursor) {
+                _("Iterating over cursor " + cursor.key + " for value " + JSON.stringify(cursor.value));
+                cursor["continue"]();
+            }
+            else {
+                ok(true, "Cursor Iteration completed");
                 db.close();
-                ok(false, "Cursor update failed");
                 start();
                 nextTest();
-            };
-            updateReq.onsuccess = function() {
-                ok(true, "Cursor update succeeded");
-                var checkReq = index.openCursor(IDBKeyRange.only(value.Int));
-                checkReq.onsuccess = function() {
-                    deepEqual(checkReq.result.value, cursorValue, "Update check succeeded");
-                    db.close();
-                    start();
-                    nextTest();
-                };
-                checkReq.onerror = function() {
-                    db.close();
-                    ok(false, "cursor check failed");
-                    start();
-                    nextTest();
-                };
-            };
-        }
-        else {
+            }
+        };
+        indexCursorReq.onerror = function(){
+            _("Error on cursor request");
             db.close();
-            ok(false, "Cursor expected");
+            ok(false, "Could not continue opening cursor");
             start();
             nextTest();
+        };
+    });
+
+    openObjectStore("Index Key Cursor", DB.OBJECT_STORE_1, function(objectStore, db){
+        var index = objectStore.index("Int Index");
+        var indexCursorReq = index.openKeyCursor();
+        indexCursorReq.onsuccess = function(){
+            var cursor = indexCursorReq.result;
+            if (cursor) {
+                _("Iterating over cursor " + cursor.key + " for value " + JSON.stringify(cursor.value));
+                cursor["continue"]();
+            }
+            else {
+                ok(true, "Cursor Iteration completed");
+                db.close();
+                start();
+                nextTest();
+            }
+        };
+        indexCursorReq.onerror = function(){
+            _("Error on cursor request");
+            db.close();
+            ok(false, "Could not continue opening cursor");
+            start();
+            nextTest();
+        };
+    });
+
+    openObjectStore("Index Get", DB.OBJECT_STORE_1, function(objectStore, db){
+        var index = objectStore.index("Int Index");
+        var req = index.get(value.Int);
+        req.onsuccess = function(){
+            deepEqual(req.result, value, "Got object from Index Get");
+            console.log("Got ", req.result, value);
+            db.close();
+            start();
+            nextTest();
+        };
+        req.onerror = function(){
+            _("Error on cursor request");
+            db.close();
+            ok(false, "Could not continue opening cursor");
+            start();
+            nextTest();
+        };
+    });
+
+
+    openObjectStore("Index Get Key", DB.OBJECT_STORE_1, function(objectStore, db){
+        var index = objectStore.index("Int Index");
+        var req = index.getKey(value.Int);
+        req.onsuccess = function(){
+            equal(req.result, key, "Got key from Index Get");
+            console.log("Got ", req.result, value);
+            db.close();
+            start();
+            nextTest();
+        };
+        req.onerror = function(){
+            _("Error on cursor request");
+            db.close();
+            ok(false, "Could not continue opening cursor");
+            start();
+            nextTest();
+        };
+    });
+
+    openObjectStore("Index update Cursor", DB.OBJECT_STORE_1, function(objectStore, db){
+        var index = objectStore.index("Int Index");
+        var kr = IDBKeyRange.only(value.Int);
+        var indexCursorReq;
+        try {
+            indexCursorReq = index.openCursor(kr);
+        } catch (err) {
+            if (err.name === 'DataError') { // PhantomJS 2.2.1 having issue here
+                // as mistakenly confusing supplied key range here with supplying
+                // key to put/add (when in-line keys are used)
+                db.close();
+                ok(false, "Cursor update failed; possibly PhantomJS 2.2.1 bug");
+                start();
+                nextTest();
+                return;
+            }
+            throw err;
         }
-    };
-    indexCursorReq.onerror = function(){
-        _("Error on cursor request");
-        db.close();
-        ok(false, "Could not continue opening cursor");
-        start();
-        nextTest();
-    };
-});
+        indexCursorReq.onsuccess = function(){
+            var cursor = indexCursorReq.result;
+            if (cursor) {
+                var cursorValue = cursor.value;
+                cursorValue.updated = true;
+                var updateReq = cursor.update(cursorValue);
+                updateReq.onerror = function() {
+                    db.close();
+                    ok(false, "Cursor update failed");
+                    start();
+                    nextTest();
+                };
+                updateReq.onsuccess = function() {
+                    ok(true, "Cursor update succeeded");
+                    var checkReq = index.openCursor(IDBKeyRange.only(value.Int));
+                    checkReq.onsuccess = function() {
+                        deepEqual(checkReq.result.value, cursorValue, "Update check succeeded");
+                        db.close();
+                        start();
+                        nextTest();
+                    };
+                    checkReq.onerror = function() {
+                        db.close();
+                        ok(false, "cursor check failed");
+                        start();
+                        nextTest();
+                    };
+                };
+            }
+            else {
+                db.close();
+                ok(false, "Cursor expected");
+                start();
+                nextTest();
+            }
+        };
+        indexCursorReq.onerror = function(){
+            _("Error on cursor request");
+            db.close();
+            ok(false, "Could not continue opening cursor");
+            start();
+            nextTest();
+        };
+    });
+}());
 
 queuedAsyncTest("Deleting Indexes", function(){
     var dbOpenRequest = window.indexedDB.open(DB.NAME, ++dbVersion);
