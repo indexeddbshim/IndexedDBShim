976e1338705c9d395c8117b613f71649d0e50fd7
Brett Zamir <brettz9@yahoo.com>
2017-04-17 12:02:43 +0800

    - Testing (W3C Browser): List out initial run of failing Safari tests

--- test-support/browser-unexpected-errors.js
+++ test-support/browser-unexpected-errors.js
@@ -1,5 +1,10 @@
 const gb = require('../test-support/node-good-bad-files');
 
 console.log(
+    'Chrome',
     [].concat(...Object.values(gb.browser.chrome)).filter((f) => gb.goodFiles.includes(f.replace(/\.html?$/, '.js')))
+);
+console.log(
+    'Safari',
+    [].concat(...Object.values(gb.browser.safari)).filter((f) => gb.goodFiles.includes(f.replace(/\.html?$/, '.js')))
 );
