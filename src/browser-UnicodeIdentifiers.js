572cb77f732da9f34d2cb0cc5c628ed2de4f4fbc
James Lal <james@lightsofapollo.com>
2017-05-02 14:43:50 -0700

    Isolate babel-polyfill into imports just for browser

--- src/browser-UnicodeIdentifiers.js
+++ src/browser-UnicodeIdentifiers.js
@@ -1,10 +1,12 @@
 /* eslint-env browser, worker */
 /* global shimIndexedDB */
+import './babel-polyfill-before';
 import * as UnicodeIdentifiers from './UnicodeIdentifiers';
 
 // BEGIN: Same code as in browser.js
 import setGlobalVars from './setGlobalVars';
 import CFG from './CFG';
+import './babel-polyfill-after';
 
 CFG.win = typeof window !== 'undefined' ? window : self; // For Web Workers
 
