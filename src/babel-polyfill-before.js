ef9173b426ac2ef4c56e0883d319f72825d8227d
Brett Zamir <brettz9@yahoo.com>
2017-05-04 11:30:24 +0800

    - Refactoring (cont'd): Remove now unused file

--- src/babel-polyfill-before.js
+++
@@ -1,2 +0,0 @@
-/* eslint-env browser, worker */
-import 'babel-polyfill'; // `Object.assign` including within `EventTarget`, generator functions, `Array.from`, etc.; see https://babeljs.io/docs/usage/polyfill/
