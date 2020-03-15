9d5d826391cf7fdeb257562488c094825b40aefb
Brett Zamir <brettz9@yahoo.com>
2020-03-15 18:36:00 +0800

    - Add source map support to Mocha tests - Remove connect and mocha grunt scripts in favor of npm ones

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,24 +57,6 @@ module.exports = function (grunt) {
                 dest: 'dist/<%=pkg.name%>-noninvasive.min.js'
             }
         },
-        connect: {
-            server: {
-                options: {
-                    base: '.',
-                    port: 9999,
-                    middleware (connect, options, middlewares) {
-                        middlewares.unshift((req, res, next) => {
-                            // Allow access to this domain from web-platform-tests so we can add the polyfill to its tests
-                            res.setHeader('Access-Control-Allow-Origin', 'http://web-platform.test:8000');
-                            res.setHeader('Access-Control-Allow-Methods', 'GET');
-                            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-                            next();
-                        });
-                        return middlewares;
-                    }
-                }
-            }
-        },
         qunit_puppeteer: {
             test: {
                 options: {
@@ -143,53 +125,6 @@ module.exports = function (grunt) {
             }
         },
 
-        mochaTest: {
-            test: {
-                options: {
-                    bail: false,
-                    require: 'source-map-support/register',
-                    timeout: 5000,
-                    reporter: 'spec',
-                    quiet: false, // Optionally suppress output to standard out (defaults to false)
-                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
-                },
-                src: ['tests-mocha/test-node.js']
-            },
-            fake: {
-                options: {
-                    bail: false,
-                    require: 'source-map-support/register',
-                    timeout: 5000,
-                    reporter: 'spec',
-                    quiet: false, // Optionally suppress output to standard out (defaults to false)
-                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
-                },
-                src: ['tests-polyfill/fakeIndexedDB/test-node.js']
-            },
-            mock: {
-                options: {
-                    bail: false,
-                    require: 'source-map-support/register',
-                    timeout: 5000,
-                    reporter: 'spec',
-                    quiet: false, // Optionally suppress output to standard out (defaults to false)
-                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
-                },
-                src: ['tests-polyfill/indexedDBmock/test-node.js']
-            },
-            w3cOld: {
-                options: {
-                    bail: false,
-                    require: 'source-map-support/register',
-                    timeout: 5000,
-                    reporter: 'spec',
-                    quiet: false, // Optionally suppress output to standard out (defaults to false)
-                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
-                },
-                src: ['tests-polyfill/w3c/test-node.js']
-            }
-        },
-
         watch: {
             all: {
                 files: ['Gruntfile.js', 'src/*', 'node_modules/eventtarget/EventTarget.js', 'node_modules/websql/lib/websql/WebSQLTransaction.js', 'node_modules/websql/lib/websql/WebSQLDatabase.js'],
@@ -235,7 +170,7 @@ module.exports = function (grunt) {
         }
     }
 
-    const testJobs = ['connect'];
+    const testJobs = [];
     grunt.registerTask('puppeteer-qunit', ['connect', 'qunit_puppeteer']);
     grunt.registerTask('mocha', ['mochaTest:test']); // clean:mochaTests isn't working here as locked (even with force:true on it or grunt-wait) so we do in package.json
     grunt.registerTask('fake', ['mochaTest:fake']);
@@ -251,8 +186,6 @@ module.exports = function (grunt) {
     grunt.registerTask('sauce-qunit', testJobs);
 
     grunt.registerTask('default', 'dev');
-    grunt.registerTask('dev', ['connect', 'watch:all']);
-    grunt.registerTask('connect-watch', ['connect', 'watch:all']);
     grunt.registerTask('dev-browser', ['connect', 'watch:browser']);
     grunt.registerTask('dev-browserNoninvasive', ['connect', 'watch:browserNoninvasive']);
     grunt.registerTask('dev-node', ['connect', 'watch:node']);
