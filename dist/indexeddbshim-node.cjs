6ff97f14cfab7ff7e8c57f4a24632762b2b65c9b
Brett Zamir <brettz9@yahoo.com>
2026-09-03 10:34:24 -0700

    build: update files

--- dist/indexeddbshim-node.cjs
+++ dist/indexeddbshim-node.cjs
@@ -1,4 +1,4 @@
-/*! indexeddbshim - v17.7.0 - 9/3/2026 */
+/*! indexeddbshim - v18.0.0 - 9/3/2026 */
 
 'use strict';
 
--- dist/indexeddbshim-noninvasive.js
+++ dist/indexeddbshim-noninvasive.js
@@ -1,4 +1,4 @@
-/*! indexeddbshim - v17.7.0 - 9/3/2026 */
+/*! indexeddbshim - v18.0.0 - 9/3/2026 */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
